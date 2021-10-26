28c1b15ec150d4c31a200dc7809ddd5a9030d654
Cüneyt <kayacuneyd@gmail.com>
2021-10-26 13:42:58 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,44 @@
+<script>
+  import LeftColumn from "./LeftColumn.svelte";
+  import RightColumn from "./RightColumn.svelte";
+</script>
+
+<!-- Page Container -->
+<div class="w3-content" style="max-width:1400px;">
+  <!-- <img
+    class="w3-image"
+    src="https://www.w3schools.com/w3css/img_snowtops.jpg"
+    alt="Norway"
+    style="width:100%"
+  /> -->
+  <!-- The Grid -->
+  <div class="w3-row">
+    <LeftColumn />
+
+    <!-- Right Column -->
+    <RightColumn />
+
+    <!-- End Grid -->
+  </div>
+
+  <!-- End Page Container -->
+</div>
+
+<footer class="w3-container w3-grey w3-center">
+  <p>Find me on social media.</p>
+  <i class="fa fa-facebook-official w3-hover-opacity" />
+  <i class="fa fa-instagram w3-hover-opacity" />
+  <i class="fa fa-snapchat w3-hover-opacity" />
+  <i class="fa fa-pinterest-p w3-hover-opacity" />
+  <i class="fa fa-twitter w3-hover-opacity" />
+  <i class="fa fa-linkedin w3-hover-opacity" />
+  <p>
+    Powered by <a
+      href="https://www.w3schools.com/w3css/default.asp"
+      target="_blank">w3.css</a
+    >
+  </p>
+</footer>
+
+<style>
+</style>
---
+++ src/LeftColumn.svelte
@@ -0,0 +1,27 @@
+<script></script>
+
+<!-- Left Column -->
+<div class="w3-third">
+  <div class="w3-white w3-text-grey w3-card-4">
+    <div class="w3-display-container ">
+      <img
+        class="w3-circle"
+        src="https://www.w3schools.com/w3images/avatar_hat.jpg"
+        style="width:100%"
+        alt="Avatar"
+      />
+
+      <h2 class="w3-center">Jane Doe</h2>
+      <p class="w3-center">Researcher / PhD. Candidate</p>
+    </div>
+    <div class="w3-container w3-grey w3-center">
+      <i class="fa fa-facebook-official w3-hover-opacity" />
+      <i class="fa fa-instagram w3-hover-opacity" />
+      <i class="fa fa-snapchat w3-hover-opacity" />
+      <br />
+    </div>
+  </div>
+  <br />
+
+  <!-- End Left Column -->
+</div>
